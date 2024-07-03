40570f1e21b965273fa2366c66933aaf3737c757
Geetal patel <geetupatel07@gmail.com>
2024-07-03 11:42:02 +0530

    third commit

---
+++ 1.Pagination/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import PaginationTest from './components/1.pagination/test';
+import './App.css'
+
+function App() {
+  return (
+    <div className='App'>
+       <PaginationTest />
+    </div>
+  )
+}
+
+export default App
